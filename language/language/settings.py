c83ce205c9a1b9038c214bcb7f256dcef95a93a6
m516 <m516@m516>
2015-10-23 16:37:41 +0800

    jj is dog

--- language/language/settings.py
+++ language/language/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'language.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'languageDB',
         'USER': 'language',
         'PASSWORD': 'language',
